228f26bfd89bb40350e88591d5bad78847a49180
Kenneth Haagen Sørensen <139569773+KenHSo@users.noreply.github.com>
2024-04-26 11:03:16 +0200

    Test of merging branches

--- GettingReal/GettingReal/Program.cs
+++ GettingReal/GettingReal/Program.cs
@@ -5,5 +5,6 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Team7 - Project Getting Real");
+        Console.WriteLine("Test af Kenneth branch");
     }
 }
