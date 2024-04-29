9547aa89b0938c64f9f9defd892d27bc1ba5787e
Kenneth Haagen Sørensen <139569773+KenHSo@users.noreply.github.com>
2024-04-29 17:39:51 +0200

    feat: Implement MVVM design pattern
    Added MVVM design typically used for big projects
    
    Added DummyClasses to folders for better overview of what the folders are for (delete these later)

---
+++ GettingReal/GettingReal.Data/Repositories/DataAccessClasses.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal.Data.Repositories
+{
+    internal class DataAccessClasses
+    {
+    }
+}
---
+++ GettingReal/GettingReal.Domain/Services/BusinessLogicHandlers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingReal.Domain.Services;
+internal class BusinessLogicHandlers
+{
+}
