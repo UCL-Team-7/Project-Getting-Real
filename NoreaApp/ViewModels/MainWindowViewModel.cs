3ba439d4c916229b3264734b5bdd28e601f6889e
FjamZoo <laurids112@live.dk>
2024-05-16 10:29:48 +0200

    Feat: File-scoped namespace

--- NoreaApp/ViewModels/MainWindowViewModel.cs
+++ NoreaApp/ViewModels/MainWindowViewModel.cs
@@ -12,156 +12,155 @@ using System.Text;
 
 #pragma warning disable CS8600, CS8604 // Converting null literal or possible null value to non-nullable type.
 
-namespace NoreaApp.ViewModels
+namespace NoreaApp.ViewModels;
+
+internal class MainWindowViewModel : ViewModelBase
 {
-    internal class MainWindowViewModel : ViewModelBase
+    public ObservableCollection<MediaFile> MediaFiles { get; set; }
+
+    private SermonRepository _sermonRepository { get; set; }
+    private IRepository _fileRepository = new FileRepository();
+    private IMetadataRepository _metadataRepository = new MetadataRepository();
+
+    //public RelayCommand AddCommand => new RelayCommand(_execute => AddMediaFile(), _canExecute => { return true; });
+    public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteMediaFile(), canExecute => SelectedItem != null);
+    public RelayCommand ExportCommand => new RelayCommand(execute => Export(), canExecute => CanExport());
+    public RelayCommand DisplayCommand => new RelayCommand(execute => Display(), canExecute => { return true; });
+    public RelayCommand UpdateCommand => new RelayCommand(execute => UpdateMediaFile(), canExecute => SelectedItem != null);
+    public RelayCommand CreateCommand => new RelayCommand(execute => CreateCustomTag(), canExecute => SelectedItem != null);
+
+    public RelayCommand DeleteCustomTagCommand => new RelayCommand(execute => DeleteCustomTag(), canExecute => SelectedItem != null);
+
+    public RelayCommand FilterListCommand => new RelayCommand(execute => FilterList(), canExecute => { return true; });
+
+    public MainWindowViewModel()
     {
-        public ObservableCollection<MediaFile> MediaFiles { get; set; }
+        MediaFiles = new ObservableCollection<MediaFile>();
+        _sermonRepository = new SermonRepository();
+    }
 
-        private SermonRepository _sermonRepository { get; set; }
-        private IRepository _fileRepository = new FileRepository();
-        private IMetadataRepository _metadataRepository = new MetadataRepository();
 
-        //public RelayCommand AddCommand => new RelayCommand(_execute => AddMediaFile(), _canExecute => { return true; });
-        public RelayCommand DeleteCommand => new RelayCommand(execute => DeleteMediaFile(), canExecute => SelectedItem != null);
-        public RelayCommand ExportCommand => new RelayCommand(execute => Export(), canExecute => CanExport());
-        public RelayCommand DisplayCommand => new RelayCommand(execute => Display(), canExecute => { return true; });
-        public RelayCommand UpdateCommand => new RelayCommand(execute => UpdateMediaFile(), canExecute => SelectedItem != null);
-        public RelayCommand CreateCommand => new RelayCommand(execute => CreateCustomTag(), canExecute => SelectedItem != null);
-
-        public RelayCommand DeleteCustomTagCommand => new RelayCommand(execute => DeleteCustomTag(), canExecute => SelectedItem != null);
-
-        public RelayCommand FilterListCommand => new RelayCommand(execute => FilterList(), canExecute => { return true; });
-
-        public MainWindowViewModel()
+    private MediaFile? _selectedItem;
+    public MediaFile? SelectedItem
+    {
+        get { return _selectedItem; }
+        set
         {
-            MediaFiles = new ObservableCollection<MediaFile>();
-            _sermonRepository = new SermonRepository();
+            _selectedItem = value;
+            OnPropertyChanged();
         }
+    }
+
+    private void FilterList()
+    {
+        SermonWindow sW = new SermonWindow();
+        sW.Show();
+    }
+
+    private void CreateCustomTag()
+    {
+        MediaFile mediaFile = SelectedItem;
+
+        int index = MediaFiles.IndexOf(mediaFile);
+
+        _metadataRepository.Create(mediaFile);
 
 
-        private MediaFile? _selectedItem;
-        public MediaFile? SelectedItem
-        {
-            get { return _selectedItem; }
-            set
-            {
-                _selectedItem = value;
-                OnPropertyChanged();
-            }
-        }
+        MediaFile updateMediaFile = _fileRepository.Read(mediaFile.Directory);
 
-        private void FilterList()
-        {
-            SermonWindow sW = new SermonWindow();
-            sW.Show();
-        }
+        MediaFiles.RemoveAt(index);
 
-        private void CreateCustomTag()
-        {
-            MediaFile mediaFile = SelectedItem;
+        MediaFiles.Insert(index, updateMediaFile);
 
-            int index = MediaFiles.IndexOf(mediaFile);
+        _sermonRepository.Create(updateMediaFile);
+    }
 
-            _metadataRepository.Create(mediaFile);
+    private void DeleteCustomTag()
+    {
+        MediaFile mediaFile = SelectedItem;
+
+        int index = MediaFiles.IndexOf(mediaFile);
+
+        _metadataRepository.DeleteCustomTag(mediaFile);
 
 
-            MediaFile updateMediaFile = _fileRepository.Read(mediaFile.Directory);
+        MediaFile updateMediaFile = _fileRepository.Read(mediaFile.Directory);
 
-            MediaFiles.RemoveAt(index);
+        MediaFiles.RemoveAt(index);
 
-            MediaFiles.Insert(index, updateMediaFile);
-
-            _sermonRepository.Create(updateMediaFile);
-        }
-
-        private void DeleteCustomTag()
-        {
-            MediaFile mediaFile = SelectedItem;
-
-            int index = MediaFiles.IndexOf(mediaFile);
-
-            _metadataRepository.DeleteCustomTag(mediaFile);
-
-
-            MediaFile updateMediaFile = _fileRepository.Read(mediaFile.Directory);
-
-            MediaFiles.RemoveAt(index);
-
-            MediaFiles.Insert(index, updateMediaFile);
-        }
-    
+        MediaFiles.Insert(index, updateMediaFile);
+    }
 
 
 
-        private void AddMediaFile()
-        {
-            throw new NotImplementedException();
 
-            //MediaFiles.Add(new MediaFile
-            //{
-            //    Title = "Not set yet",
-            //    Artist = "Not set yet",
-            //    Comment = "Not set yet",
-            //});
+    private void AddMediaFile()
+    {
+        throw new NotImplementedException();
 
-        }
-
-        public void Display()
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-
-            dialog.Multiselect = true;
-
-            bool? userClicksOK = dialog.ShowDialog();
-
-            if (userClicksOK == true)
-            {
-                MediaFiles.Clear();
-                MediaFiles = _fileRepository.ReadAll(dialog.FileNames);
-
-                foreach (MediaFile file in MediaFiles)
-                {
-                    if (file.NoreaType == "Prædiken")
-                    {
-                        _sermonRepository.Create(file);
-                    }
-                }
-
-                OnPropertyChanged(nameof(MediaFiles));
-            }
-
-            // if () ikke har alle customtags
-            // så kald metadata repo
-            // create som laver alle vores customtags, og sætter dem til empty string
-
-
-        }
-
-        private void Export()
-        {
-            //Save to file/db
-
-            string[] headers = ["TITEL", "KUNSTNER", "ALBUM", "ÅR", "SPOR", "ALBUMSKUNSTNER", "GENRE", "KOMMENTAR", "FILSTI", "KOMPONIST"];
-
-            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MediaFiles.csv"), false, Encoding.UTF8))
-            {
-
-                string headerLine = string.Join(";", headers);
-                sw.WriteLine(headerLine);
-
-                foreach (MediaFile file in MediaFiles)
-                {
-                    sw.WriteLine(file);
-                }
-            }
-
-        }
-
-        private bool CanExport() => MediaFiles.Any();
-        private void DeleteMediaFile() => MediaFiles.Remove(SelectedItem);
-        private void UpdateMediaFile() => _fileRepository.Update(SelectedItem);
+        //MediaFiles.Add(new MediaFile
+        //{
+        //    Title = "Not set yet",
+        //    Artist = "Not set yet",
+        //    Comment = "Not set yet",
+        //});
 
     }
+
+    public void Display()
+    {
+        OpenFileDialog dialog = new OpenFileDialog();
+
+        dialog.Multiselect = true;
+
+        bool? userClicksOK = dialog.ShowDialog();
+
+        if (userClicksOK == true)
+        {
+            MediaFiles.Clear();
+            MediaFiles = _fileRepository.ReadAll(dialog.FileNames);
+
+            foreach (MediaFile file in MediaFiles)
+            {
+                if (file.NoreaType == "Prædiken")
+                {
+                    _sermonRepository.Create(file);
+                }
+            }
+
+            OnPropertyChanged(nameof(MediaFiles));
+        }
+
+        // if () ikke har alle customtags
+        // så kald metadata repo
+        // create som laver alle vores customtags, og sætter dem til empty string
+
+
+    }
+
+    private void Export()
+    {
+        //Save to file/db
+
+        string[] headers = ["TITEL", "KUNSTNER", "ALBUM", "ÅR", "SPOR", "ALBUMSKUNSTNER", "GENRE", "KOMMENTAR", "FILSTI", "KOMPONIST"];
+
+        using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MediaFiles.csv"), false, Encoding.UTF8))
+        {
+
+            string headerLine = string.Join(";", headers);
+            sw.WriteLine(headerLine);
+
+            foreach (MediaFile file in MediaFiles)
+            {
+                sw.WriteLine(file);
+            }
+        }
+
+    }
+
+    private bool CanExport() => MediaFiles.Any();
+    private void DeleteMediaFile() => MediaFiles.Remove(SelectedItem);
+    private void UpdateMediaFile() => _fileRepository.Update(SelectedItem);
+
 }
 #pragma warning restore CS8600, CS8604 // Converting null literal or possible null value to non-nullable type.
